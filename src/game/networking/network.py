9eb00094331c20d6565abc158c23f484074cb5c9
J0pper <104348534+J0pper@users.noreply.github.com>
2024-05-20 23:05:24 +0200

    Some polishing.

--- src/game/networking/network.py
+++ src/game/networking/network.py
@@ -5,7 +5,6 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a new socket per client.
-        # self.server = socket.gethostbyname(socket.gethostname())  # Grabs your local IPv4 address.
         self.server: str = input("Server IP-address here: ")
         self.port: int = int(input("Port here: "))  # Specify a free port on your internet connection.
         self.connectMessage: str = self.connect_server()  # Connects the client socket with the server.
--- src/game/networking/server.py
+++ src/game/networking/server.py
@@ -5,18 +5,19 @@ from src.game.board_manager import BoardManager
 
 
 def start_server():
-    server = socket.gethostbyname(socket.gethostname())
-    print("Server IP-address: ", server)
-    portRange = [5000, 7000]
+    ipAddress = socket.gethostbyname(socket.gethostname())  # get local ip-address for the device running the server.
+    print("Server IP-address: ", ipAddress)
+    portRange = [5000, 7000]  # specify a range within to check for available ports.
     port = portRange[0]
-    binding = True
 
     # Create the socket for the server.
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Try binding to a port until it succeeds, then break the while loop.
+    binding = True
     while binding:
         try:
-            s.bind((server, port))
+            s.bind((ipAddress, port))
             binding = False
             print("Server port: ", port)
         except socket.error as e:
@@ -25,49 +26,53 @@ def start_server():
                 continue
             print("no ports available", e)
 
+    # Tell the socket to only listen for two connections.
     s.listen(2)
     print("Waiting for a connection, Server Started")
 
     # Amount of players or clients connected to the server.
     # Default is 0, but the number increases with one per connected client.
     currPlayerAmount: int = 0
-    boardManager = None
+    boardManager = BoardManager()  # Holds a BoardManager-object.
     while True:
-        conn, addr = s.accept()
+        # Accept a connection to the socket.
+        conn, addr = s.accept()  # Returns a new socket representing the connection, and the address of the client.
         print("Connected to:", addr)
 
-        if currPlayerAmount % 2 != 1:
-            boardManager = BoardManager()
-        else:
+        # Set the connectReady variable to true when to players have connected.
+        if currPlayerAmount % 2 == 1:
             boardManager.connectReady = True
 
+        # Start a new thread and supply it with a function to run.
         start_new_thread(threaded_client, (conn, currPlayerAmount, boardManager))
         currPlayerAmount += 1
 
 
 def threaded_client(connection, player, board_manager):
-    connection.send(str.encode(str(player)))
+    connection.send(str.encode(str(player)))  # Send the player number to the player.
 
     while True:
         try:
-            data = pickle.loads(connection.recv(4096*8))
+            data = pickle.loads(connection.recv(4096*8))  # Try to retrieve and load data.
             if not data:
                 print("Didn't receive data. Disconnected as a result.")
                 break
+            # Check what the data was.
             if data[0] == "ready":
                 board_manager.ready(player, data[1])
             if data == "flip_turn":
                 board_manager.flip_turn()
             if data != "get":
                 board_manager.update_boards(player, data)
-            connection.sendall(pickle.dumps(board_manager))
+            connection.sendall(pickle.dumps(board_manager))  # Send a pickled BoardManager-object.
 
+        # Handle errors
         except socket.error as err:
             print("Error occurred in threaded-client", err)
             break
 
     print("Lost connection")
-    connection.close()
+    connection.close()  # Close the client socket.
 
 
 start_server()
